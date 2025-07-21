603afb3bb4af326ac898818970d347ac71638bc7
oM- <blowman158@gmail.com>
2025-07-21 23:22:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import discord
 from discord import app_commands
 from discord.ext import commands, tasks
 from datetime import datetime, timedelta, timezone
-from webserver import keep_alive
 import asyncio
 import os
 
